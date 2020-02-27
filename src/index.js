ae4d2c1efc580d17778535f46bb743a6bbdffab1
raent25 <raent25@gmail.com>
2020-02-27 03:08:21 +0200

    layout of blocks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './modules/header.component'
 import './modules/burger'
+import './modules/slider'
 import css from './css/index.css'
 import './scss/index.scss'
--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -1,2 +1,9 @@
 import $ from 'jquery'
-
+$(document).ready(function() {
+    $("body").on("click", "a", function(event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
