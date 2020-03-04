cb9096b8bc601f051431029a8531eb9bf8de4285
raent25 <raent25@gmail.com>
2020-03-04 13:16:55 +0200

    main part

--- src/modules/burger.js
+++ src/modules/burger.js
@@ -1,28 +1,39 @@
 class Burger {
-    constructor(button, overley, link) {
+    constructor(button, overley, link, body) {
+
+
+
         document.querySelector(button).onclick = function() {
             document.querySelector('.burger-menu__line').classList.toggle('burger-menu-active')
             document.querySelector('#nav').classList.toggle('burger-menu-active')
-            document.querySelector('#nav').classList.toggle('width-content')
             document.querySelector('.burger-menu__overley').classList.toggle('burger-menu-active')
             document.querySelector('.burger-menu__button').classList.toggle('burger-menu-active')
-
-
         }
         document.querySelector(overley).onclick = function() {
-            document.querySelector('.burger-menu__line').classList.toggle('burger-menu-active')
-            document.querySelector('#nav').classList.toggle('burger-menu-active')
-            document.querySelector('#nav').classList.toggle('width-content')
-            document.querySelector('.burger-menu__overley').classList.toggle('burger-menu-active')
-            document.querySelector('.burger-menu__button').classList.toggle('burger-menu-active')
+            document.querySelector('.burger-menu__line').classList.remove('burger-menu-active')
+            document.querySelector('#nav').classList.remove('burger-menu-active')
+            document.querySelector('.burger-menu__overley').classList.remove('burger-menu-active')
+            document.querySelector('.burger-menu__button').classList.remove('burger-menu-active')
         }
         document.querySelector(link).onclick = function() {
-            document.querySelector('.burger-menu__line').classList.toggle('burger-menu-active')
-            document.querySelector('#nav').classList.toggle('burger-menu-active')
-            document.querySelector('#nav').classList.toggle('width-content')
-            document.querySelector('.burger-menu__overley').classList.toggle('burger-menu-active')
-            document.querySelector('.burger-menu__button').classList.toggle('burger-menu-active')
+            document.querySelector('.burger-menu__line').classList.remove('burger-menu-active')
+            document.querySelector('#nav').classList.remove('burger-menu-active')
+            document.querySelector('.burger-menu__overley').classList.remove('burger-menu-active')
+            document.querySelector('.burger-menu__button').classList.remove('burger-menu-active')
+        }
+
+
+
+        document.querySelector(body).onresize = function() {
+            this.$width = document.querySelector(body).clientWidth
+            if (this.$width > 1140) {
+                document.querySelector('.burger-menu__line').classList.remove('burger-menu-active')
+                document.querySelector('#nav').classList.remove('burger-menu-active')
+                document.querySelector('.burger-menu__overley').classList.remove('burger-menu-active')
+                document.querySelector('.burger-menu__button').classList.remove('burger-menu-active')
+
+            }
         }
     }
 }
-const burger = new Burger('#button', '#overley', '#nav')
+const burger = new Burger('#button', '#overley', '#nav', 'body')
